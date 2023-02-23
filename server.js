795e99009a996503b42395f554c47809f8a55cc8
Ignale <ignat.lex@gmail.com>
2023-02-23 18:48:57 +0300

    make it better

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  console.log(req)
-  res.send('Hello World!')
-})
+const app = express();
 
-app.post('/', (req, res) => {
-  console.log(req)
-})
+// Enable CORS to allow cross-origin requests from WooCommerce
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+});
 
-app.put('/', (req, res) => {
-  console.log(req)
-})
+// Parse incoming webhook data as JSON
+app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Example app listening on port`)
-})
+// Handle incoming WooCommerce webhook requests
+app.post('/woocommerce-webhook', (req, res) => {
+  // Get the webhook data from the request body
+  const webhookData = req.body;
+
+  // Process the webhook data as needed
+  console.log('Received WooCommerce webhook:', webhookData);
+
+  // Send a response to WooCommerce to confirm receipt of the webhook data
+  res.status(200).send('Webhook received');
+});
+
+// Start the Express.js server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Express.js server listening on port ${port}`);
+});

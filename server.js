ce13aee9b3e0664ff31a53d0d8fb20ed574fb8ac
Ignale <ignat.lex@gmail.com>
2023-02-25 20:38:48 +0300

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const querystring = require('querystring')
 const request = require('request')
-const https = require('https')
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 const app = express()
 
 
